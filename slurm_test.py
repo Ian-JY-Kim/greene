2b0c9f02e3dfec40f5f9df498acd5518ec23bac3
Ian-JY-Kim <87853188+Ian-JY-Kim@users.noreply.github.com>
2023-07-18 16:46:14 -0400

    Add files via upload

--- slurm_test.py
+++ slurm_test.py
@@ -1,4 +1,16 @@
 import sys
+import numpy as np
+import pandas as pd
+from pandas import DataFrame as df
+import random
+from scipy.optimize import fsolve
+from scipy.optimize import minimize
+import math
+from scipy.stats import norm
+from statistics import NormalDist
+from scipy.integrate import quad
+from itertools import product
+
 
 print("task %d is done" %int(sys.argv[1]))
 
