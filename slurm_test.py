b49f76c1dbe7db887945093d9c1ad6f5e1856006
Ian-JY-Kim <87853188+Ian-JY-Kim@users.noreply.github.com>
2023-07-18 16:27:31 -0400

    Add files via upload

--- slurm_test.py
+++ slurm_test.py
@@ -1,7 +1,7 @@
 import sys
 
-print("task %f is done" %sys.argv[1])
+print("task %d is done" %int(sys.argv[1]))
 
 txt_file = open("test.txt", "a")
-txt_file.write("%f" %sys.argv[1])
+txt_file.write("%d" %int(sys.argv[1]))
 txt_file.close()
