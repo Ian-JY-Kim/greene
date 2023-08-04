59fd8200cb269e80f2c724884f2e0dd756b07cfb
Ian-JY-Kim <87853188+Ian-JY-Kim@users.noreply.github.com>
2023-08-03 23:37:01 -0400

    Add files via upload

---
+++ slurm_main_job_v5.s
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=myJobarrayTest
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+#SBATCH --mem=1GB
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jk7362@nyu.edu
+
+module purge
+module load python/intel/3.8.6
+
+cd /home/jk7362/greene
+python slurm_main_job_v5.py $SLURM_ARRAY_TASK_ID
